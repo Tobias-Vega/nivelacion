4c866fad0bcefe584880c3c1e9af9df4a11bccde
Tobias <tobiasvega1210@gmail.com>
2025-03-19 13:01:49 -0300

    Eliminacion de carpetaa

--- backend/src/controllers/colors.controller.js
+++
@@ -1,6 +0,0 @@
-import { colorsArr } from '../data/colors.js'
-
-export const getColorsCtrl = (_req, res) => {
-
-  res.json(colorsArr);
-}
--- backend/src/routes/colors.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { getColorsCtrl } from '../controllers/colors.controller.js';
-
-export const colorRouter = Router();
-
-colorRouter.get('/colors', getColorsCtrl);
